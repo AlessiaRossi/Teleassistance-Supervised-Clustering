776b853ae5840082f14ab15ee079fb1b464caad7
MartinaBertazzoni <martina.bertazzoni.bmh@gmail.com>
2024-07-25 17:24:31 +0200

    Added print_details_corrections function

--- data_prep/feature_selection.py
+++ data_prep/feature_selection.py
@@ -14,3 +14,22 @@ coppie_colonne = [
     ('codice_tipologia_struttura_erogazione', 'tipologia_struttura_erogazione'),
     ('codice_tipologia_professionista_sanitario', 'tipologia_professionista_sanitario')
 ]
+
+def print_details_corrections(df, codice, descrizione, gruppi_codice, gruppi_descrizione):
+    '''
+    Print details if there are codes with multiple descriptions or vice versa
+    :param df: DataFrame to operate on
+    :param codice: Code column to analyze
+    :param descrizione: Description column to be parsed
+    :param gruppi_codice: Code groups with unique description counts
+    :param gruppi_descrizione: Groups of descriptions with unique code counts
+    '''
+    for cod, num_desc in gruppi_codice.items():
+        if num_desc > 1:
+            descrizioni_associate = df[df[codice] == cod][descrizione].unique()
+            print(f"The {cod} code is associated with {num_desc} descriptions:: {descrizioni_associate}")
+
+    for desc, num_cod in gruppi_descrizione.items():
+        if num_cod > 1:
+            codici_associati = df[df[descrizione] == desc][codice].unique()
+            print(f"The description '{desc}' is associated with {num_cod} codes: {codici_associati}")
