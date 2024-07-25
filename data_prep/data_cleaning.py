2b9ed1fce8f66285ce6999de3fb55ac6614621bc
MartinaBertazzoni <martina.bertazzoni.bmh@gmail.com>
2024-07-25 16:05:50 +0200

    Added identify_and_remove_outliers function

--- data_prep/data_cleaning.py
+++ data_prep/data_cleaning.py
@@ -2,9 +2,25 @@ import pandas as pd
 
 def remove_disdette(df) -> pd.DataFrame:
     """
-    Rimuove i campioni con 'data_disdetta' non nullo.
+    Removes samples with non-zero 'date_deadline'.
     :param df:
-    :return: df senza campioni con 'data_disdetta' non nullo.
+    :return: df without samples with non-zero 'date_date'.
     """
     df = df[df['data_disdetta'].isnull()]
+    return df
+
+def identify_and_remove_outliers(df, columns):
+    """
+    Identifies and removes outliers using the IQR method.
+    :param df: The original DataFrame.
+    :param columns: The columns on which to apply outliers removal.
+    :return: A DataFrame with no outliers.
+    """
+    for column in columns:
+        Q1 = df[column].quantile(0.25)
+        Q3 = df[column].quantile(0.75)
+        IQR = Q3 - Q1
+        lower_bound = Q1 - 1.5 * IQR
+        upper_bound = Q3 + 1.5 * IQR
+        df = df[(df[column] >= lower_bound) & (df[column] <= upper_bound)]
     return df
