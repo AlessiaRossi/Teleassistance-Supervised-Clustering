21c9a39f26666e86ae0538ba3ec34012d9c5f261
MartinaBertazzoni <martina.bertazzoni.bmh@gmail.com>
2024-07-25 17:03:50 +0200

    Created the feature_selection.py script and Added the list of tuples

---
+++ data_prep/feature_selection.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+# List of tuples containing the code-description column pairs to be compared.
+coppie_colonne = [
+    ('codice_provincia_residenza', 'provincia_residenza'),
+    ('codice_provincia_erogazione', 'provincia_erogazione'),
+    ('codice_regione_residenza', 'regione_residenza'),
+    ('codice_asl_residenza', 'asl_residenza'),
+    ('codice_comune_residenza', 'comune_residenza'),
+    ('codice_descrizione_attivita', 'descrizione_attivita'),
+    ('codice_regione_erogazione', 'regione_erogazione'),
+    ('codice_asl_erogazione', 'asl_erogazione'),
+    ('codice_struttura_erogazione', 'struttura_erogazione'),
+    ('codice_tipologia_struttura_erogazione', 'tipologia_struttura_erogazione'),
+    ('codice_tipologia_professionista_sanitario', 'tipologia_professionista_sanitario')
+]
