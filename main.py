63fdf644a802dd1e61753889662ae444efd1938d
Ignazio-Emanuele-Picciche <ignazioemanuele.picciche@gmail.com>
2024-08-09 16:30:06 +0200

    added comments

--- main.py
+++ main.py
@@ -12,14 +12,12 @@ df = pd.read_parquet(file_path)
 num_rows, num_columns = df.shape
 print(f"The DataFrame has {num_rows} rows and {num_columns} columns.\n")
 
+'''
+    The DataFrame has 484291 rows and 33 columns.
+'''
+
+
 print('\nNULLS BEFORE DATA CLEANING\n', df.isnull().sum().sort_values(ascending=False))
-
-# Data Cleaning
-df = data_cleaning_execution(df)
-
-
-
-print('\nNULLS AFTER DATA CLEANING\n', df.isnull().sum().sort_values(ascending=False))
 
 '''
     NULLS BEFORE DATA CLEANING
@@ -31,6 +29,22 @@ print('\nNULLS AFTER DATA CLEANING\n', df.isnull().sum().sort_values(ascending=F
     ora_inizio_erogazione                         28181
     comune_residenza                                135
 '''
+
+# Data Cleaning
+df = data_cleaning_execution(df)
+
+print('\nNULLS AFTER DATA CLEANING\n', df.isnull().sum().sort_values(ascending=False))
+
+"""
+    NULLS AFTER DATA CLEANING
+
+    data_disdetta                                460639
+    codice_provincia_erogazione                   27396
+    codice_provincia_residenza                    27016
+    comune_residenza                                130
+    dtype: int64
+-----------------------------------
+"""
 
 
 
@@ -46,34 +60,6 @@ df = feature_selection_execution(df)
 num_rows, num_columns = df.shape
 print(f"The DataFrame has {num_rows} rows and {num_columns} columns.")
 
-
-
 '''
-    Statistiche valori mancanti dopo l'imputazione:
-
-    codice_provincia_residenza      28380
-    comune_residenza                  135 --> da non toccare perché relativi al comune di None in provincia di Torino
-    codice_provincia_erogazione     28776
-    ora_inizio_erogazione           23652
-    ora_fine_erogazione             23652
-    data_disdetta                  460639
+    The DataFrame has 460639 rows and 22 columns.
 '''
-
-# TODO: rimuovere colonne relative a 'codice_provincia_residenza' e 'codice_provincia_erogazione' in quanto non utili
-
-# TODO: rimuovere tutti i campioni che hanno una data_disdetta non nulla poiché non significativi dato che la
-#  televisita non è avvenuta ma è stata annullata. In questo modo non ci saranno valori mancanti per 'ora_inizio_erogazione' e 'ora_fine_erogazione'
-
-# TODO: rimuovere colonne relative a 'data_disdetta' in quanto non utili. Dopo il secondo TODO per quella feature ci saranno solo valori mancanti
-
-"""
-    DOPO TODO:
-    Statistiche valori mancanti dopo l'imputazione:
-
-    data_disdetta                                460639
-    codice_provincia_erogazione                   27396
-    codice_provincia_residenza                    27016
-    comune_residenza                                130
-    dtype: int64
------------------------------------
-"""
