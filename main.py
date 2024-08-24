1e60af495b9e140173299271de461adcd3dc85a5
Ignazio-Emanuele-Picciche <ignazioemanuele.picciche@gmail.com>
2024-08-24 16:57:42 +0200

    initial main setup

--- main.py
+++ main.py
@@ -1,72 +1,96 @@
 import pandas as pd
 from src.data_prep.data_cleaning import data_cleaning_execution
 from src.data_prep.feature_selection import feature_selection_execution
+import yaml
+import logging
 
-#from feature_extraction.feature_extraction import feature_extraction
+def load_config(config_file):
+    """Load the configuration file."""
+    with open(config_file, 'r') as file:
+        config = yaml.safe_load(file)
+    return config
 
-# Loading the dataset
-file_path = 'data/raw/challenge_campus_biomedico_2024.parquet'
-df = pd.read_parquet(file_path)
-
-# Displaying the number of rows and columns in the dataset
-num_rows, num_columns = df.shape
-print(f"The DataFrame has {num_rows} rows and {num_columns} columns.\n")
-
-'''
-    The DataFrame has 484291 rows and 33 columns.
-'''
+def load_data():
+    pass
 
 
-print('\nNULLS BEFORE DATA CLEANING\n', df.isnull().sum().sort_values(ascending=False))
+def main():
 
-'''
-    NULLS BEFORE DATA CLEANING
+    # Load the configuration file
+    config = load_config('conifg.yaml')
 
-    data_disdetta                                460639
-    codice_provincia_erogazione                   28776
-    codice_provincia_residenza                    28380
-    ora_fine_erogazione                           28181
-    ora_inizio_erogazione                         28181
-    comune_residenza                                135
-'''
+    # Set up logging
+    # level=logging.INFO
+    logging.basicConfig(filename=config['general']['logging_level'], format='%(asctime)s - %(message)s')
 
-# Data Cleaning
-df = data_cleaning_execution(df)
+    # Load the data
+    file_path = config['data']['file_path']
+    df = pd.read_parquet(file_path)
 
-print('\nNULLS AFTER DATA CLEANING\n', df.isnull().sum().sort_values(ascending=False))
-
-"""
-    NULLS AFTER DATA CLEANING
-
-    data_disdetta                                460639
-    codice_provincia_erogazione                   27396
-    codice_provincia_residenza                    27016
-    comune_residenza                                130
-    dtype: int64
------------------------------------
-"""
+    # Display the number of rows and columns in the dataset
+    '''
+        The DataFrame has 484291 rows and 33 columns.
+    '''
+    num_rows, num_columns = df.shape
+    logging.info(f"The DataFrame has {num_rows} rows and {num_columns} columns.\n")
 
 
+    logging.info('\nNULLS BEFORE DATA CLEANING\n', df.isnull().sum().sort_values(ascending=False))
+    '''
+        NULLS BEFORE DATA CLEANING
 
-# Features Selection
-df = feature_selection_execution(df)
+        data_disdetta                                460639
+        codice_provincia_erogazione                   28776
+        codice_provincia_residenza                    28380
+        ora_fine_erogazione                           28181
+        ora_inizio_erogazione                         28181
+        comune_residenza                                135
+    '''
 
-# Feature extraction
-#df = feature_extraction(df)
+    # Phase 1: Data Cleaning
+    if config['cleaning']['cleaning_enabled']:
+        missing_threshold = config['cleaning']['missing_threshold']
 
-#df.to_csv('data/raw/challenge_campus_biomedico_2024_imputed_selected_extracted.csv', index=False)
+        df = data_cleaning_execution(df)
 
-# Displaying the number of rows and columns in the dataset
-num_rows, num_columns = df.shape
-print(f"The DataFrame has {num_rows} rows and {num_columns} columns.")
+        logging.info('\nNULLS AFTER DATA CLEANING\n', df.isnull().sum().sort_values(ascending=False))
+        '''
+            NULLS AFTER DATA CLEANING
 
-'''
-    The DataFrame has 460639 rows and 22 columns.
-'''
+            data_disdetta                                460639
+            codice_provincia_erogazione                   27396
+            codice_provincia_residenza                    27016
+            comune_residenza                                130
+            dtype: int64
+        '''
+
+        cleaned_file_path = config['data']['cleaned_file_path']
+        df.to_parquet(cleaned_file_path)
+
+    else:
+        cleaned_file_path = config['data']['cleaned_file_path']
+        df = pd.read_parquet(cleaned_file_path)
 
 
-print('\nNULLS AFTER FEATURE SELECTION\n', df.isnull().sum().sort_values(ascending=False))
-'''
-NULLS AFTER FEATURE SELECTION
- comune_residenza                      130
-'''
+    # Phase 2: Feature Selection
+    if config['feature_selection']['selection_enabled']:
+        df = feature_selection_execution(df)
+
+        logging.info('\nNULLS AFTER FEATURE SELECTION\n', df.isnull().sum().sort_values(ascending=False))
+        '''
+            NULLS AFTER FEATURE SELECTION
+            comune_residenza                      130
+        '''
+
+        feature_selected_file_path = config['data']['selected_file_path']
+        df.to_parquet(feature_selected_file_path)
+
+    else:
+        feature_selected_file_path = config['data']['selected_file_path']
+        df = pd.read_parquet(feature_selected_file_path)
+
+
+if __name__ == '__main__':
+    main()
+
+
