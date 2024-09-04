edd2d826513cadc3bd636137c2d347a8f7971cca
Ignazio-Emanuele-Picciche <ignazioemanuele.picciche@gmail.com>
2024-09-04 19:19:03 +0200

    added comments to improve code understanding

--- src/metrics_evaluation.py
+++ src/metrics_evaluation.py
@@ -9,11 +9,21 @@ import matplotlib.pyplot as plt
 
 
 def purity_score(true_labels:pd.Series, clusters:pd.Series) -> float:
-    """Calculate the purity score."""
+    '''
+        This function calculates the purity score of a clustering algorithm.
+
+        Parameters:
+        - true_labels: The true labels of the data points.
+        - clusters: The cluster labels assigned by the clustering algorithm.
+
+        Returns:
+        - The purity score of the clustering algorithm.
+    '''
     
     N = len(true_labels) # number of data points
     purity_sum = 0
 
+    # Iterate over each cluster
     for cluster_id in np.unique(clusters):
         # Obtain the indices of data points in the cluster
         # print(cluster_id)
@@ -31,11 +41,23 @@ def purity_score(true_labels:pd.Series, clusters:pd.Series) -> float:
 
         purity_sum += count
 
-    purity = purity_sum / N
+    purity = purity_sum / N # Calculate the purity score
     return purity
 
 
 def silhouette_score(features, clusters) -> float:
+    '''
+        This function calculates the silhouette score of a clustering algorithm.
+
+        Parameters:
+        - features: The feature data used for clustering.
+        - clusters: The cluster labels assigned by the clustering algorithm.
+
+        Returns:
+        - The silhouette score of the clustering algorithm.
+    '''
+
+
     # Calculate the silhouette scores for each sample
     silhouette_vals = silhouette_samples(features, clusters)
     
@@ -55,7 +77,16 @@ def silhouette_score(features, clusters) -> float:
 
 
 def label_encoding(df:pd.DataFrame):
-    """Label encoding of the categorical columns."""
+    '''
+        This function performs label encoding on non-numeric columns in the DataFrame.
+
+        Parameters:
+        - df: DataFrame containing the labeled data for clustering.
+
+        Returns:
+        - X_standardized_df: DataFrame containing the standardized feature data.
+        - clusters: The cluster labels assigned by the clustering algorithm.
+    '''
 
     # Extract clusters and true labels
     clusters = df['cluster'].to_numpy()
@@ -69,8 +100,9 @@ def label_encoding(df:pd.DataFrame):
             le = LabelEncoder()
             feature_columns[column] = le.fit_transform(feature_columns[column])
             
-    # NOTE Sarebbe meglio usare OneHotEncoder ma avendo un dataset molto grande non posso permettermi di fare one hot encoding in quanto la memoria non basta
+    # NOTE It would be better to use OneHotEncoder, but since I have a very large dataset, I cannot afford to do one-hot encoding as it would exceed the memory limit.
 
+    # Standardize the feature data
     scaler = StandardScaler()
     X_standardized = scaler.fit_transform(feature_columns)
 
@@ -80,25 +112,48 @@ def label_encoding(df:pd.DataFrame):
 
 
 def plot_silhouette(silhouette_vals, clusters, num_init):
+    '''
+        This function plots the silhouette values for each sample, grouped by cluster.
+
+        Parameters:
+        - silhouette_vals: The silhouette values for each sample.
+        - clusters: The cluster labels assigned by the clustering algorithm.
+    '''
+
     # Plot the silhouette values for each sample
     plt.figure(figsize=(10, 7))
     y_lower = 10
-    for i in np.unique(clusters):
-        ith_cluster_silhouette_vals = silhouette_vals[clusters == i]
-        ith_cluster_silhouette_vals.sort()
-        size_cluster_i = ith_cluster_silhouette_vals.shape[0]
-        y_upper = y_lower + size_cluster_i
 
+    # Iterate over each cluster
+    for i in np.unique(clusters):
+        ith_cluster_silhouette_vals = silhouette_vals[clusters == i] # Get the silhouette values for the current cluster
+        ith_cluster_silhouette_vals.sort() # Sort the silhouette values
+        size_cluster_i = ith_cluster_silhouette_vals.shape[0]   # Get the size of the cluster
+        y_upper = y_lower + size_cluster_i  # Calculate the upper limit for the cluster plot
+
+        # Fill the area for each cluster
         plt.fill_betweenx(np.arange(y_lower, y_upper), 0, ith_cluster_silhouette_vals, alpha=0.7)
         plt.text(-0.05, y_lower + 0.5 * size_cluster_i, str(i))
 
+        # Compute the new y_lower for the next plot
         y_lower = y_upper + 10
 
     plt.savefig(f'graphs/{num_init}_silhouette_plot.png')
 
 
 def metrics_execution(df:pd.DataFrame, config:dict, num_init:str) -> float:
-    """Execution of metrics calculation."""
+    '''
+        This function calculates the purity and silhouette scores of a clustering algorithm.
+
+        Parameters:
+        - df: DataFrame containing the labeled data for clustering.
+        - config: Dictionary containing configuration settings for the clustering process.
+
+        Returns:
+        - The purity score of the clustering algorithm.
+        - The mean normalized silhouette score of the clustering algorithm.
+        - The final metric, with a penalty for the number of clusters, of the clustering algorithm.
+    '''
     
     purity = purity_score(df['incremento_teleassistenze'], df['cluster'])
     logging.info(f'Purity score: {purity}')
@@ -110,9 +165,11 @@ def metrics_execution(df:pd.DataFrame, config:dict, num_init:str) -> float:
 
     # Plot the silhouette values for each sample
     plot_silhouette(silhouette_vals, clusters, num_init)
+    logging.info(f'Silhouette plot saved as {num_init}_silhouette_plot.png')
 
     # Calculate the final metrics with a penalty for the number of clusters
     final_metric = ((purity + mean_normalized_silhouette_vals) / 2) - (0.05 * config['modelling_clustering']['n_clusters'])
+    logging.info(f'Final metric: {final_metric}')
 
     return purity, mean_normalized_silhouette_vals, final_metric
     
